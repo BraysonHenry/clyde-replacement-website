b49cdef35aed65e89aa4bbd3d57e7eba0e8d8372
Brayson (Kid) <brayson.henry.2025@gmail.com>
2026-08-29 18:54:30 -0400

    Implement NextAuth with Discord provider

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,28 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || "1543122832723152937",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || "t76EPuq1JHW_mFiGODZ1xsDSGMyxdpu_",
+      authorization: { params: { scope: "identify guilds" } },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: any; token: any }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
